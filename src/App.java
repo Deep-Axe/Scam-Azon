3bf8656382edee6a8dcbc5076a5d5d80ed89b260
Deep <133337403+Deep-Axe@users.noreply.github.com>
2024-11-06 16:13:40 +0530

    Main file to be run

--- src/App.java
+++ src/App.java
@@ -2,18 +2,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Scam-Azon");
-
-        // Initialize the login page directly
+        primaryStage.setTitle("Scam-Azon");   
         new LoginPage(primaryStage);
-
-        // Show the primary stage
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
